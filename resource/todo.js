931ab383c176e4d36438730951da3c3247ffca70
sali-web <salina.gurung009@gmail.com>
2022-10-12 17:28:02 +0545

    Add to list, Edit and Delete were updated

---
+++ resource/todo.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        "_id": "001",
+        "title": "Complete Bootcamp Project",
+        "description": "Talk to team, make team repository, assign task"
+    },
+    {
+        "_id": "002",
+        "title": "Complete the slides for presentation",
+        "description": "Do research, gather resources and prepare notes"
+    },
+    {
+        "_id": "003",
+        "title": "Go to the driving class",
+        "description": "Wake up early and take 1 hour class"
+    },
+    {
+        "_id": "004",
+        "title": "Meet friends",
+        "description": "Hangout and have lunch together"
+    }
+]
+
+module.exports = todos;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var todos = require('../resource/todo');
+//console.log(todos)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Salina'});
+  res.render('home',{todosList:todos});
 });
 
 router.get('/home',function(req,res,next){
   res.render('home');
+})
+
+router.get('/add-to-do',function(req,res,next){
+  res.render('addTodo',{title:"Add to do"})
+})
+
+router.post('/save-to-do',function(req,res,next){
+  todos.push({...req.body,_id: `00${todos.length}`});
+  res.redirect('/');
+})
+
+// router.get('/delete-to-do/:index', function(req,res,next){
+//   console.log(req.params.index);
+//   todos.splice(req.params.index,1);
+//   res.redirect('/');
+// })
+
+router.get('/delete-to-do/:id', function(req,res,next){
+  console.log(req.params.id);
+  todos.splice(req.params.id,1);
+  res.redirect('/');
+})
+
+router.get('/open-update-form/:id',function(req,res,next){
+  const todotodo = todos.find(todo => todo._id === req.params.id);
+  res.render('editToDo',{todo:todotodo});
+})
+
+router.post('/update-to-do/:id',function(req,res,next){
+ // console.log(req.body,req.params);
+  const index = todos.findIndex(todo => todo._id === req.params.id);
+  todos.splice(index,1,{...req.body,_id: req.params.id});
+  res.redirect('/');
 })
 
 module.exports = router;
